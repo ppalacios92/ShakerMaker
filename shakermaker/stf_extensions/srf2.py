48d630398697b4eec4abc365896b3b2bdb8ee5da
Patricio Palacios B. <pxpalacios@gmail.com>
2026-03-07 18:29:56 -0300

    SRF2 function

--- shakermaker/stf_extensions/srf2.py
+++ shakermaker/stf_extensions/srf2.py
@@ -8,18 +8,20 @@ class SRF2(SourceTimeFunction):
     Implements the provided srf2 functional form as a slip rate function.
     
     Parameters:
-        Tp : float
-            The parameter Tp from the srf2 function.
-        Tr : float
-            The parameter Tr from the srf2 function.
+        Tr, Tp, Te : float
+            The parameter from the srf2 function.
         dt : float
             Time step for the source time function.
     """
-    def __init__(self, Tp, Tr, dt):
+    def __init__(self, Tr, Tp, Te, dt, a, b):
         SourceTimeFunction.__init__(self)
         self._Tp = Tp
         self._Tr = Tr
+        self._Te = Te
         self._dt = dt
+        self._a = a
+        self._b = b
+
     
     def _generate_data(self):
         t, svf = self._srf2_function()
@@ -27,9 +29,8 @@ class SRF2(SourceTimeFunction):
         self._data = svf
 
     def _srf2_function(self):
-        Te = 0.7 * self._Tr
-        a = 1.
-        b = 100.
+        a = self._a
+        b = self._b
 
         t = np.arange(0, self._Tr, self._dt)
         Nt = len(t)
@@ -37,9 +38,9 @@ class SRF2(SourceTimeFunction):
 
         i1 = t < self._Tp
         svf[i1] = t[i1] / self._Tp * np.sqrt(a + b / self._Tp**2) * np.sin(np.pi * t[i1] / (2 * self._Tp))
-        i2 = np.logical_and(self._Tp <= t, t < Te)
+        i2 = np.logical_and(self._Tp <= t, t < self._Te)
         svf[i2] = np.sqrt(a + b / t[i2]**2)
-        i3 = t >= Te
+        i3 = t >= self._Te
         svf[i3] = np.sqrt(a + b / t[i3]**2) * np.sin(5/3 * np.pi * (self._Tr - t[i3]) / self._Tr)
 
         A = np.trapz(svf, dx=self._dt)
